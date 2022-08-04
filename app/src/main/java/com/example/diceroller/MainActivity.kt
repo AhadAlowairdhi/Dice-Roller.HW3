1006c42b652a00863cbdc9c6359739c1b760f58d
Ahad <odisha2226@gmail.com>
2022-08-04 13:42:57 +0300

    Added (Images & Dice Class & Rolling function)

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,22 +3,47 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+
+/**
+ * This activity allows the user
+ * to interact with the screen (UI)
+ * */
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // must
         setContentView(R.layout.activity_main)
         //declare & initialize UI
         val rollBtn = findViewById<Button>(R.id.btn1)
-        val tvDice = findViewById<TextView>(R.id.tv1)
+
 
         rollBtn.setOnClickListener {
-            //Show Toast
-            val toast = Toast.makeText(this, "Dice rolled",Toast.LENGTH_SHORT)
-            toast.show()
+            rollDice()
         }
     }
+
+    private fun rollDice(){
+        // create object
+        val dice = Dice(6)
+        // save the result number
+        val diceRoll = dice.roll()
+
+        // reference to imageView
+        val diceImage : ImageView = findViewById(R.id.img_dice)
+
+        // check number and update screen (UI)
+        when(diceRoll){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        } // End when
+    } // End rollDice() function
 }
